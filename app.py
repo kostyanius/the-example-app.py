44e761fd09abcf2dd35509e2f3fdedaf2939d603
Kostyantyn Khomko <kostyanius@gmail.com>
2021-03-05 20:27:47 +0200

    Update app.py
    adding listen all hosts paramater

--- app.py
+++ app.py
@@ -93,7 +93,7 @@ def server_error(e):
     raise e
 
 if __name__ == '__main__':
-    app.run(
+    app.run(host='0.0.0.0',
         port=int(os.environ.get('PORT', DEFAULT_PORT)),
         threaded=True
     )
